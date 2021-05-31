2ea18c58b9a9264678efc11e5ab2282426d8fe4a
Hanchut <Galoorellana2@hotmail.com>
2021-05-31 19:23:10 -0300

    Update - 2.9 - Ejercicio 10
    Finalizado.

--- Ejercicios/tp4ej10.py
+++ Ejercicios/tp4ej10.py
@@ -7,10 +7,23 @@
 
 # Reemplazar por las funciones del ejercicio
 
+from tp4ej1 import ingreso_entero
+
+def factores_primos(numero):
+    
+    numeros_primos = []
+    for contador in range(2, numero + 1):
+        while numero % contador == 0:
+            numeros_primos.append(contador)
+            numero = numero / contador
+    return tuple(numeros_primos)
 
 def prueba():
     
-    pass
+    print('Bienvenido!')
+    ingreso = ingreso_entero('\nIngrese un numero entero para saber sus factores primos:')
+    numeros_primos = factores_primos(ingreso)
+    print(f'\nFactores primos de {ingreso}:', numeros_primos)
 
 if __name__ == "__main__":
     prueba()
