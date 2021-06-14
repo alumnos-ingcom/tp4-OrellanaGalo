426a9c0fc9d600b77e42893eb2c392c9788804a2
Hanchut <Galoorellana2@hotmail.com>
2021-06-14 10:25:24 -0300

    Update Final - Ejercicio 1
    Ultimo Commit del TP4. Todos los ejercicios fueron corregidos siendo el ultimo faltante el 4. Subo para revisiones.

--- Ejercicios/tp4ej1.py
+++ Ejercicios/tp4ej1.py
@@ -7,25 +7,44 @@
 
 # Reemplazar por las funciones del ejercicio
 
-def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
-    
-    """ Donde comienza // Cuando termina // Cuanto avanza """ 
-    
-    for i in range(0, cantidad_reintentos, 1):
-        ingreso_entero("Puede ingresar 5 valores enteros.") 
-
-def ingreso_entero_restringido(mensaje, valor_minimo=0, valor_maximo=10):
-
-    print(mensaje)
-    i = int(input("Numero a ingresar: "))
-    if i < valor_minimo or i > valor_maximo:
-        print ("Por favor. Solo ingresar numeros entre 0 y 10")
-    elif i >= valor_minimo or i <= valor_maximo:
-        print ("El numero ingresado es correcto. Muchas gracias por su colaboracion.")
-
 class IngresoIncorrecto(Exception):
     """Esta es la excepcion para el ingreso incorrecto"""
     pass
+
+class SinIntentos(Exception):
+    """Esta es la excepcion para el reintento de ingreso en la funcion 'ingreso_entero_reintento'."""
+    pass
+
+class SinParametro(Exception):
+    """Esta es la excepcion para establecer los parametros en la funcion 'ingreso_entero_restringido'."""
+    pass
+
+def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
+    
+    intentos = 0
+    entero = None
+    
+    while entero == None and intentos < cantidad_reintentos:
+        entero = ingreso_entero(mensaje)
+        intentos = intentos - 1
+        if entero == None:
+            print('Intentos restantes: ', intentos)
+    if intentos == cantidad_reintentos:
+        raise SinIntentos('Se han terminado las oportunidades de reintentos.')
+    return entero
+
+def ingreso_entero_restringido(mensaje, valor_minimo, valor_maximo):
+
+    entero = ingreso_entero(mensaje)
+    
+    try:
+        if entero >= valor_minimo and entero <= valor_maximo:
+            return entero
+        else:
+            raise SinParametro('El numero ingresado se encuentra fuera de los parametros establecidos.')
+            pass
+    except TypeError as err:
+        raise TypeError ('Ingrese solo numeros enteros.') from err
 
 def ingreso_entero(mensaje):
     
@@ -33,20 +52,20 @@ def ingreso_entero(mensaje):
     Esta funcion muestra un mensaje y agrega la # para indicar el ingreso
     de un numero entero.
     """
-    ingreso = input(mensaje + " #")
     try:
-        entero = int(ingreso)
+        entero = int(input(mensaje + " #"))
+        return entero
     except ValueError as err:
         raise IngresoIncorrecto("No era un numero!") from err
-    return entero
+        return entero
 
 def prueba():
     
-    print(mensaje)
-    ingreso_entero_reintento("Tenes 5 intentos para agregar un numero entero")
-    ingreso_entero_restringido("Ingrese un numero entre 0 y 10")
-    
-    pass
+    print('Bienvenido a mi codigo!')
+    ingreso_usuario = ingreso_entero_reintento("\nTenes 5 intentos para agregar un numero entero")
+    ingreso_usuario = ingreso_entero_restringido("Ingrese un numero entre 0 y 10", 0, 10)
+    print(f'El numero ingresado fue: {ingreso_usuario}')
+
 
 if __name__ == "__main__":
     prueba()
